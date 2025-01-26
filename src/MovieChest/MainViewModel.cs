c48dd5fc0eded91a58fa118f193662371f47c189
Adam Labranche <adam@labranche.co>
2025-01-26 17:48:13 -0500

    Allow searching for movies

--- src/MovieChest/MainViewModel.cs
+++ src/MovieChest/MainViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MovieChest.ComponentModel;
+using System;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace MovieChest;
 
@@ -20,9 +23,22 @@ public partial class MainViewModel : ViewModelBase
     {
         Movies.Add(new MovieItem { Title = "Kung Pow", Description = "Best movie ever." });
         Movies.Add(new MovieItem { Title = "Up", Description = "Best animation movie ever." });
+
+        this.When()
+            .Any([nameof(MovieFilter)], UpdateFilteredMovies)
+            .Subscribe()
+            .DisposeWith(this);
+
+        filteredMovies = GetFilteredMovies();
     }
 
     public ObservableCollection<MovieItem> Movies { get; } = [];
+
+    [ObservableProperty]
+    private string? movieFilter;
+
+    [ObservableProperty]
+    private ImmutableArray<MovieItem> filteredMovies = [];
 
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(DeleteSelectedMovieCommand))]
@@ -37,8 +53,17 @@ public partial class MainViewModel : ViewModelBase
         }
         Movies.Remove(selectedMovie);
         SelectedMovie = null;
+        UpdateFilteredMovies();
     }
 
     private bool CanDeleteSelectedMovie()
         => SelectedMovie is not null;
+
+    private ImmutableArray<MovieItem> GetFilteredMovies()
+        => string.IsNullOrWhiteSpace(MovieFilter)
+        ? Movies.ToImmutableArray()
+        : Movies.Where(x => x.Title.Contains(MovieFilter, StringComparison.CurrentCultureIgnoreCase)).ToImmutableArray();
+
+    private void UpdateFilteredMovies()
+        => FilteredMovies = GetFilteredMovies();
 }
