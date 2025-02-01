acb401e0cef6bbe0533027bea9462e7b30b7d4ab
Adam Labranche <adam@labranche.co>
2025-02-01 07:45:39 -0500

    Create EditMovieViewModel

---
+++ src/MovieChest/EditMovieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MovieChest.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieChest;
+
+public partial class EditMovieViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    [Required]
+    [MinLength(1)]
+    [MaxLength(100)]
+    private string title = "";
+
+    [ObservableProperty]
+    [Required]
+    [MaxLength(2000)]
+    private string description = "";
+}
